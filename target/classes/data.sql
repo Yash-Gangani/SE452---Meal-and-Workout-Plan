8a84f422582dcc9b909f35ae28e6e16475b49e06
johnsmillie <jsmillie23@gmail.com>
2024-05-19 17:09:30 -0500

    data.sql added for seeded data - meals

---
+++ target/classes/data.sql
@@ -0,0 +1,5 @@
+insert into nutrition (calories, fat, sugar, id) values (1000,20,14,1);
+insert into nutrition (calories, fat, sugar, id) values (1500,25,12,2);
+
+insert into meal(id,t,nutrition_id) values (1,null, 1);
+insert into meal(id,t,nutrition_id) values (2,null,2);
