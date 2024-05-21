140e8d3f3f0c6bd410f8a2c0da15606bd997bc25
johnsmillie <jsmillie23@gmail.com>
2024-05-20 19:38:54 -0500

    import BeforeEach

--- src/test/java/com/mealplan/project/mealplan/MealPlanServiceTest.java
+++ src/test/java/com/mealplan/project/mealplan/MealPlanServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.*;
 import java.util.Arrays;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
