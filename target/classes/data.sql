cf9f8779113f338b4c124f494baceddaf03ce1a5
johnsmillie <jsmillie23@gmail.com>
2024-05-24 20:49:51 -0500

    added gitignore, deleted target folder

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into nutrition (calories, fat, sugar, id) values (1000,20,14,1);
-insert into nutrition (calories, fat, sugar, id) values (1500,25,12,2);
--- fix this to handle Enum MealType
-insert into meal(id,t,nutrition_id) values (1,null,1);
-insert into meal(id,t,nutrition_id) values (2,null,2);
