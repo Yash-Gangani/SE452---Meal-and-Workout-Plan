b259c46abd53f2bc093221a39c04ebfc51c2a523
johnsmillie <jsmillie23@gmail.com>
2024-05-31 21:22:30 -0500

    Changed to Crud repo from jpa to facilitate redis db if needed.

--- src/main/java/com/mealplan/project/person/PersonRepository.java
+++ src/main/java/com/mealplan/project/person/PersonRepository.java
@@ -1,9 +1,7 @@
 package com.mealplan.project.person;
 
+import org.springframework.data.repository.CrudRepository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
+public interface PersonRepository extends CrudRepository<Person, Integer> {
   
 }
