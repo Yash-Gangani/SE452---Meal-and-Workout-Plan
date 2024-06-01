560c3a2025e25a57daf3d60a1f5fb4af6d0c265b
johnsmillie <jsmillie23@gmail.com>
2024-06-01 02:58:47 -0500

    changed to crud repo

--- src/main/java/com/mealplan/project/meal/MealRepository.java
+++ src/main/java/com/mealplan/project/meal/MealRepository.java
@@ -1,7 +1,8 @@
 package com.mealplan.project.meal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface MealRepository extends JpaRepository<Meal,Integer>{
+public interface MealRepository extends CrudRepository<Meal,Integer>{
   
 }
