c5dedeca31e675e7a85635e202b3e3afc272fbc1
johnsmillie <jsmillie23@gmail.com>
2024-05-31 19:28:42 -0500

    Added Id auto-generation back into meal class

--- src/main/java/com/mealplan/project/meal/Meal.java
+++ src/main/java/com/mealplan/project/meal/Meal.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Meal {
   @Id
-  //@GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private MealType t;
   @OneToOne
