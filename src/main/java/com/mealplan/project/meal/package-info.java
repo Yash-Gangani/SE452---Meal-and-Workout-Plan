9c15f6344c5f2926ef58bc8b60dcca990334f28b
johnsmillie <jsmillie23@gmail.com>
2024-06-10 13:03:04 -0500

    Descriptive package-info files

--- src/main/java/com/mealplan/project/meal/package-info.java
+++ src/main/java/com/mealplan/project/meal/package-info.java
@@ -1,4 +1,15 @@
 package com.mealplan.project.meal;
+
+
+
 /*
  * John Smillie
+ * - Meal.java: simple data object
+ * - MealType.java: type enum
+ * - MealRepository.java: CRUD repository
+ * - MealService: CRUD service calls with logging and exception handling
+ * - MealResource: REST Controller wrapper for MealService.java
+ * - MealController: Model/View controller for HTTP method calls
+ * Additional Feature:
+ * - Uses /resources/templates/meal/list.html template resource (Thymeleaf Template Engine)
  */
--- src/main/java/com/mealplan/project/person/package-info.java
+++ src/main/java/com/mealplan/project/person/package-info.java
@@ -1,4 +1,12 @@
 package com.mealplan.project.person;
 /*
  * John Smillie
+ * - Person.java: simple data object
+ * - Gender.java: type enum
+ * - PersonRepository.java: CRUD repository
+ * - PersonService: CRUD service calls with logging and exception handling
+ * - PersonResource: REST Controller wrapper for PersonService.java
+ * - PersonController: Model/View controller for HTTP method calls
+ * Additional Feature:
+ * - Uses /resources/templates/person/list.html template resource (Thymeleaf Template Engine)
  */
